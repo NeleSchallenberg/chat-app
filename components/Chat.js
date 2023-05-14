28600a420c53016d68ac7d2e4ea9c89fe3a84ff8
NeleSchallenberg <nschallenberg@gmx.net>
2023-05-14 23:13:28 +0200

    Fetch messages from the database in real time

--- components/Chat.js
+++ components/Chat.js
@@ -9,17 +9,25 @@ import {
 } from 'react-native';
 // Import Gifted Chat library
 import { GiftedChat, Bubble } from 'react-native-gifted-chat';
+// Import firebase functions
+import {
+	collection,
+	onSnapshot,
+	query,
+	where,
+	orderBy,
+	addDoc,
+} from 'firebase/firestore';
 
 // Create chat screen
 const Chat = ({ route, navigation, db }) => {
 	const { name, backgroundColor } = route.params;
+	const { userID } = route.params;
 	// Initialize messages state
 	const [messages, setMessages] = useState([]);
 	// Call function when user sends message
 	const onSend = (newMessages) => {
-		setMessages((previousMessages) =>
-			GiftedChat.append(previousMessages, newMessages)
-		);
+		addDoc(collection(db, 'messages'), newMessages[0]);
 	};
 	// Create renderBubble function
 	const renderBubble = (props) => {
@@ -38,30 +46,75 @@ const Chat = ({ route, navigation, db }) => {
 		);
 	};
 
+	// useEffect(() => {
+	// 	navigation.setOptions({ title: name });
+	// 	const q = query(
+	// 		collection(db, 'messages'),
+	// 		orderBy('createdAt', 'desc')
+	// 	);
+	// 	const unsubMessages = onSnapshot(q, (docs) => {
+	// 		let newMessages = [];
+	// 		docs.forEach((doc) => {
+	// 			newMessages.push({
+	// 				id: doc.id,
+	// 				...doc.data(),
+	// 				createdAt: new Date(doc.data().createdAt.toMillis()),
+	// 			});
+	// 		});
+	// 		setMessages(newMessages);
+	// 	});
+	// 	return () => {
+	// 		if (unsubMessages) unsubMessages();
+	// 	};
+	// }, []);
+
 	useEffect(() => {
 		// Display username in navigation header
 		navigation.setOptions({ title: name });
+
+		// Create listener on query that targets messages collection
+		const q = query(
+			collection(db, 'messages'),
+			orderBy('createdAt', 'desc')
+		);
+		const unsubChat = onSnapshot(q, (documentsSnapshot) => {
+			let newMessages = [];
+			documentsSnapshot.forEach((doc) => {
+				newMessages.push({
+					id: doc.id,
+					...doc.data(),
+					createdAt: new Date(doc.data().createdAt.toMillis()),
+				});
+			});
+			setMessages(newMessages);
+		});
+
+		// Clean up code
+		return () => {
+			if (unsubChat) unsubChat();
+		};
+
 		// Set state with static message
-		setMessages([
-			// User message
-			{
-				_id: 1,
-				text: `Hey ${name}, welcome to the chat!`,
-				createdAt: new Date(),
-				user: {
-					_id: 2,
-					name: 'React Native',
-					avatar: 'https://robohash.org/2a02:3033:404:a55a:295a:3115:d5eb:b603.png',
-				},
-			},
-			// System message
-			{
-				_id: 2,
-				text: `${name} has joined the group`,
-				createdAt: new Date(),
-				system: true,
-			},
-		]);
+		// setMessages([
+		// 	// User message
+		// 	{
+		// 		_id: 1,
+		// 		text: `Hey ${name}, welcome to the chat!`,
+		// 		createdAt: new Date(),
+		// 		user: {
+		// 			_id: 2,
+		// 			name: 'React Native',
+		// 			avatar: 'https://robohash.org/2a02:3033:404:a55a:295a:3115:d5eb:b603.png',
+		// 		},
+		// 	},
+		// 	// System message
+		// 	{
+		// 		_id: 2,
+		// 		text: `${name} has joined the group`,
+		// 		createdAt: new Date(),
+		// 		system: true,
+		// 	},
+		// ]);
 	}, []);
 
 	return (
@@ -70,10 +123,12 @@ const Chat = ({ route, navigation, db }) => {
 		<View style={styles.container}>
 			<GiftedChat
 				messages={messages}
+				backgroundColor={backgroundColor}
 				renderBubble={renderBubble}
 				onSend={(messages) => onSend(messages)}
 				user={{
-					_id: 1,
+					_id: userID,
+					name: name,
 				}}
 			/>
 			{Platform.OS === 'android' ? (
